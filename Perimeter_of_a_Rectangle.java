8098211614fe0be3b2ed07b04257c416c126834d
Satyam Gupta <Satyamgupta9012@gmail.com>
2025-06-10 17:27:58 +0530

    basic

---
+++ Perimeter_of_a_Rectangle.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main{
+	public static void main(String [] args){
+		Scanner sc=new Scanner(System.in);
+		int l=sc.nextInt(),w=sc.nextInt();
+		System.out.println(2*(l+w));
+	}
+}
